52913432f06b1f77124898620e2498bf283b3888
kotsiuk dmitro <kotsiukdmitro@gmail.com>
2025-02-05 17:59:49 +0200

    improved the page work portfolio

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,55 +4,55 @@ const fs = require('fs')
 const { createRemoteFileNode } = require("gatsby-source-filesystem")
 
 exports.createSchemaCustomization = ({ actions }) => {
-    const { createTypes } = actions
+  const { createTypes } = actions
 
-    createTypes(`
+  createTypes(`
     type node__article implements Node {
       body_images: [File] @link(from: "fields.localFile")
     }
   `)
 }
 
-exports.onCreateNode = async ({node, actions, createNodeId, getCache}) => {
-    const { createNode, createNodeField } = actions
+exports.onCreateNode = async ({ node, actions, createNodeId, getCache }) => {
+  const { createNode, createNodeField } = actions
 
-    if (node.internal.type === 'node__article') {
-        const {id: nodeId, body: { value: bodyValue }} = node
+  if (node.internal.type === 'node__article') {
+    const { id: nodeId, body: { value: bodyValue } } = node
 
-        const findImgRegEx = /<img [^>]*src="[^"]*"[^>]*>/gm
-        const replaceSrcRegEx = /.*src="([^"]*)".*/
+    const findImgRegEx = /<img [^>]*src="[^"]*"[^>]*>/gm
+    const replaceSrcRegEx = /.*src="([^"]*)".*/
 
-        let images = bodyValue
-            .match(findImgRegEx) || []
+    let images = bodyValue
+      .match(findImgRegEx) || []
 
-        images = images.map(x => x.replace(replaceSrcRegEx, new URL('$1', process.env.GATSBY_API_BASE_URL)))
+    images = images.map(x => x.replace(replaceSrcRegEx, new URL('$1', process.env.GATSBY_API_BASE_URL)))
 
-        const fileIdList = []
+    const fileIdList = []
 
-        for (let image of images) {
-            const fileNode = await createRemoteFileNode({
-                url: image,
-                parentNodeId: nodeId,
-                createNode,
-                createNodeId,
-                getCache
-            })
+    for (let image of images) {
+      const fileNode = await createRemoteFileNode({
+        url: image,
+        parentNodeId: nodeId,
+        createNode,
+        createNodeId,
+        getCache
+      })
 
-            if (fileNode) {
-                fileIdList.push(fileNode.id)
-            }
-        }
-
-        createNodeField({ node, name: "localFile", value: fileIdList })
+      if (fileNode) {
+        fileIdList.push(fileNode.id)
+      }
     }
+
+    createNodeField({ node, name: "localFile", value: fileIdList })
+  }
 }
 
 
-exports.createPages = async ({graphql, actions, reporter}) => {
-    const {createPage} = actions
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
 
-    // Validation
-    const allNodes = await graphql(`
+  // Validation
+  const allNodes = await graphql(`
       query {
         allNodePage(filter: {status: {eq: true}}) {
           nodes {
@@ -89,26 +89,26 @@ exports.createPages = async ({graphql, actions, reporter}) => {
         }
       }
     `)
-    const metatagsValidation = (nodes, nodeType = 'страницы') => {
-        nodes.map(node => {
-            if (!node.field_metatags) {
-                reporter.panicOnBuild(`❌ Никакие из SEO параметров не заполнены для ${nodeType} с названием ${node.title}`)
-            }
+  const metatagsValidation = (nodes, nodeType = 'страницы') => {
+    nodes.map(node => {
+      if (!node.field_metatags) {
+        reporter.panicOnBuild(`❌ Никакие из SEO параметров не заполнены для ${nodeType} с названием ${node.title}`)
+      }
 
-            const {title, description, og_title, og_description} = node.field_metatags
-            if (!title || !description || !og_title || !og_description) {
-                reporter.panicOnBuild(`❌ Некоторые из SEO параметров(${!title ? 'title/' : ''}${!description ? 'description/' : ''}${!og_title ? 'og_title/' : ''}${!og_description ? 'og_description' : ''}) не заполнены для ${nodeType} с названием ${node.title}`)
-            }
-        })
-    }
+      const { title, description, og_title, og_description } = node.field_metatags
+      if (!title || !description || !og_title || !og_description) {
+        reporter.panicOnBuild(`❌ Некоторые из SEO параметров(${!title ? 'title/' : ''}${!description ? 'description/' : ''}${!og_title ? 'og_title/' : ''}${!og_description ? 'og_description' : ''}) не заполнены для ${nodeType} с названием ${node.title}`)
+      }
+    })
+  }
 
-    reporter.info('Начало валидации')
-    metatagsValidation(allNodes.data.allNodePage.nodes)
-    metatagsValidation(allNodes.data.allNodeOurWork.nodes, 'проекта')
-    metatagsValidation(allNodes.data.allNodeArticle.nodes, 'статьи блога')
-    reporter.info('Валидация прошла успешно')
+  reporter.info('Начало валидации')
+  metatagsValidation(allNodes.data.allNodePage.nodes)
+  metatagsValidation(allNodes.data.allNodeOurWork.nodes, 'проекта')
+  metatagsValidation(allNodes.data.allNodeArticle.nodes, 'статьи блога')
+  reporter.info('Валидация прошла успешно')
 
-    const projectIds = await graphql(`
+  const projectIds = await graphql(`
       query {
         allNodeOurWork(filter: {status: {eq: true}}) {
           edges {
@@ -121,44 +121,56 @@ exports.createPages = async ({graphql, actions, reporter}) => {
                 robots
               }
             }
+            previous {
+              path {
+                alias
+              }
+            }
+            next {
+              path {
+                alias
+              }
+            }
           }
         }
       }
     `)
-      const regexp = /noindex|nofollow|noarchive|nosnippet|noimageindex|notranslate/ig
-      projectIds.data.allNodeOurWork.edges.forEach(edge => {
-          createPage({
-              path: edge.node.path.alias,
-              component: path.resolve(`src/templates/SingleProject/single-project.js`),
-              context: {
-                  id: edge.node.id,
-                  noindex: regexp.test(edge.node.field_metatags.robots)
-              }
-          })
-      })
+  const regexp = /noindex|nofollow|noarchive|nosnippet|noimageindex|notranslate/ig
+  projectIds.data.allNodeOurWork.edges.forEach(edge => {
+    createPage({
+      path: edge.node.path.alias,
+      component: path.resolve(`src/templates/SingleProject/single-project.js`),
+      context: {
+        id: edge.node.id,
+        noindex: regexp.test(edge.node.field_metatags.robots),
+        prevProject: edge.previous ? edge.previous.path.alias : null,
+        nextProject: edge.next ? edge.next.path.alias : null,
+      }
+    })
+  })
 
 }
 
 exports.onPostBuild = () => {
-    if (fs.existsSync('./public/sitemap-0.xml')) {
-        fs.renameSync('./public/sitemap-0.xml', './public/sitemap.xml')
-    }
+  if (fs.existsSync('./public/sitemap-0.xml')) {
+    fs.renameSync('./public/sitemap-0.xml', './public/sitemap.xml')
+  }
 }
 
 exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
-    if (stage === `build-javascript`) {
-        actions.setWebpackConfig({
-            devtool: false,
+  if (stage === `build-javascript`) {
+    actions.setWebpackConfig({
+      devtool: false,
 
-        })      
-    }
+    })
+  }
 
-    if (stage === 'build-javascript' || stage === 'develop') {
-      const config = getConfig()
-      const miniCssExtractPlugin = config.plugins.find(
-          plugin => (plugin.constructor.name === 'MiniCssExtractPlugin')
-      )
-      if (miniCssExtractPlugin) miniCssExtractPlugin.options.ignoreOrder = true
-      actions.replaceWebpackConfig(config)
+  if (stage === 'build-javascript' || stage === 'develop') {
+    const config = getConfig()
+    const miniCssExtractPlugin = config.plugins.find(
+      plugin => (plugin.constructor.name === 'MiniCssExtractPlugin')
+    )
+    if (miniCssExtractPlugin) miniCssExtractPlugin.options.ignoreOrder = true
+    actions.replaceWebpackConfig(config)
   }
 }
--- src/templates/SingleProject/single-project.js
+++ src/templates/SingleProject/single-project.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react'
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Slider from 'react-slick'
 import { apStyleTitleCase } from 'ap-style-title-case'
@@ -9,12 +9,17 @@ import { checkInfiniteSliderMode } from '../../helpers/checkInfiniteSliderMode'
 import { ProjectLink, ProjectVideo } from '../../components/Templates'
 import { route } from '../../routes'
 import CustomTitleComponent from '../../components/common/CustomTitleComponent/CustomTitleComponent'
-import './single-project-slider.css'
+import arrowBack from '../../assets/images/arrows/arrow-back.svg'
+import arrowPrev from '../../assets/images/arrows/prev.svg'
+import arrowNext from '../../assets/images/arrows/next.svg'
 import * as style from './single-project.module.css'
+import './single-project-slider.css'
 
 
 
-const SingleProject = ({ data }) => {
+const SingleProject = ({ data, pageContext }) => {
+
+    const { prevProject, nextProject } = pageContext
 
     const singleProjectSlider = useRef(null)
     const {
@@ -75,29 +80,47 @@ const SingleProject = ({ data }) => {
         ]
     }
 
-    useEffect(() => {
-        const startAutoplay = setTimeout(() => {
-            singleProjectSlider.current.slickPlay()
-        }, 3000)
+    // useEffect(() => {
+    //     const startAutoplay = setTimeout(() => {
+    //         singleProjectSlider.current.slickPlay()
+    //     }, 3000)
 
-        return () => {
-            clearTimeout(startAutoplay)
-        }
-    }, [])
+    //     return () => {
+    //         clearTimeout(startAutoplay)
+    //     }
+    // }, [])
 
     return (
         <Layout isTransparent={true}>
             <div className={['mx-auto px-0 md:px-5', style.singleProjectPage].join(' ')}>
-                <AnchorLink className={'text-[#24d5d1] mt-60 2xl:pt-32'} to={route('home.portfolio')}>
+                <AnchorLink className={'text-[#24d5d1] items-center inline-flex mt-5 ml-5 md:ml-0'} to={route('home.portfolio')}>
+                    <img src={arrowBack} alt='arrow-back' className={'mr-2'} />
                     <span>Back to Portfolio</span>
                 </AnchorLink>
-                <div className={'flex flex-col-reverse md:flex-col'}>
 
-                    <div
-                        className={[style.headerLeftContent, 'px-4 md:px-0 w-full flex flex-col items-start md:items-baseline md:justify-between md:pt-10'].join(' ')}>
-                        <div className='text-center'><CustomTitleComponent name={apStyleTitleCase(title)} /></div>
-                        <ProjectLink viewLink={viewLink} />
+                <div className={'flex mt-6 gap-5 text-[14px] text-[#9f9f9f] justify-center'}>
+                    {prevProject && (
+                        <Link to={prevProject} className={'flex'}>
+                            <img src={arrowPrev} alt='arrow-prev' className={'mr-2'} />
+                            <span className=''>Previous Project</span>
+                        </Link>
+                    )}
+                    {nextProject && (
+                        <Link to={nextProject} className={'flex'}>
+                            <span>Next Project</span>
+                            <img src={arrowNext} alt='arrow-next' className={'ml-2'} />
+                        </Link>
+                    )}
+                </div>
+
+                <div className={'flex flex-col-reverse md:flex-col'}>
+                    <div className={'px-4 md:px-0 w-full flex flex-col pt-10 lg:mb-10'}>
+                        <CustomTitleComponent name={apStyleTitleCase(title)} />
+                        {viewLink && <div className={'mt-5 mb-5 md:mb-0'}>
+                            <ProjectLink viewLink={viewLink} />
+                        </div>}
                     </div>
+                    
                     <div className={'w-full mb-0 md:mb-14'}>
                         <div className={'-mt-2 md:mt-0'}>
                             <Slider {...settings} ref={singleProjectSlider}>
@@ -119,6 +142,7 @@ const SingleProject = ({ data }) => {
                         </div>
                     </div>
                 </div>
+
                 <div className={['px-4 md:px-0', style.singleProjectContent].join(' ')}>
                     <hr className={style.breakLine} />
                     <div className={style.fieldWrap}>
@@ -170,7 +194,6 @@ const SingleProject = ({ data }) => {
                         </div>
                     </div>
                     <hr className={style.breakLine} />
-
                 </div>
             </div>
         </Layout>
